3ce75aa60113149940e1153009f6a5fee6dfcf86
Jaden Geller <jaden.geller@gmail.com>
2015-10-26 16:21:07 -0700

    Added xcode project

---
+++ Graham/Graham/Graham.h
@@ -0,0 +1,19 @@
+//
+//  Graham.h
+//  Graham
+//
+//  Created by Jaden Geller on 10/26/15.
+//  Copyright © 2015 Jaden Geller. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Graham.
+FOUNDATION_EXPORT double GrahamVersionNumber;
+
+//! Project version string for Graham.
+FOUNDATION_EXPORT const unsigned char GrahamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graham/PublicHeader.h>
+
+
